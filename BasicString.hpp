36535ba54029aad862b60d3cd393abb88d403adc
Shriram V <shri314@yahoo.com>
2024-03-19 08:12:56 +0530

    add substr, replace, insert, find, add tests

--- BasicString.hpp
+++ BasicString.hpp
@@ -24,6 +24,8 @@ template<typename CharT>
 class BasicString
 {
 public:
+    static const size_t npos = -1;
+
     BasicString() = default;
 
     BasicString(const CharT* buf, size_t sz)
@@ -75,17 +77,27 @@ public:
 
     void clear()
     {
-        m_size = 0; // std::destroy....
+        // std::destroy(data(), data() + size());
+        m_size = 0;
     }
 
     BasicString& assign(const CharT* buf, size_t sz)
     {
-        if(sz > capacity() || !std::is_nothrow_constructible_v<CharT>)
+        auto assign_exsafe_ = [&]()
         {
             using std::swap;
 
             BasicString tmp{buf, sz};
             swap(*this, tmp);
+        };
+
+        if constexpr(!std::is_nothrow_constructible_v<CharT>)
+        {
+            assign_exsafe_();
+        }
+        else if(sz > capacity())
+        {
+            assign_exsafe_();
         }
         else
         {
@@ -106,9 +118,16 @@ public:
         return assign(rhs.data(), rhs.size());
     }
 
+    //////////////////////////
+
     BasicString& operator=(const BasicString& rhs)
     {
         return assign(rhs.data(), rhs.size());
+    }
+
+    BasicString& operator=(const CharT* buf)
+    {
+        return assign(buf);
     }
 
     BasicString& operator=(BasicString&& rhs)
@@ -141,6 +160,8 @@ public:
 
     BasicString& append(const CharT* buf, size_t sz)
     {
+        if(sz == 0) return *this; // quick return (minor optimization)
+
         if(sz > spare_capacity_())
         {
             size_t new_cap = std::max(
@@ -213,21 +234,21 @@ public:
 
     const CharT& operator[](size_t index) const
     {
-        assert(index < m_size);
+        assert(index < size());
 
         return m_data[index];
     }
 
     CharT& operator[](size_t index)
     {
-        assert(index < m_size);
+        assert(index < size());
 
         return m_data[index];
     }
 
     const CharT& at(size_t index) const
     {
-        if(index >= m_size)
+        if(index >= size())
             throw std::out_of_range{"bad index"};
 
         return m_data[index];
@@ -235,7 +256,7 @@ public:
 
     CharT& at(size_t index)
     {
-        if(index >= m_size)
+        if(index >= size())
             throw std::out_of_range{"bad index"};
 
         return m_data[index];
@@ -259,6 +280,161 @@ public:
     CharT& back()
     {
         return (*this)[size() - 1];
+    }
+
+    //////////////////////////
+
+    BasicString substr(size_t index = 0, size_t count = npos) const
+    {
+        if(index >= size())
+            throw std::out_of_range{"bad index"};
+
+        return BasicString{data() + index, std::min(size() - index, count)};
+    }
+
+    BasicString& replace(size_t index, size_t count, const CharT* buf, size_t sz)
+    {
+        if(index >= size())
+            throw std::out_of_range{"bad index"};
+
+        size_t eff_count = std::min(size() - index, count);
+        size_t new_size = add_sat_(size() - eff_count, sz);
+
+        if(new_size > max_size())
+            throw std::length_error("size too big");
+
+        auto replace_exsafe_ = [&]()
+        {
+            using std::swap;
+
+            BasicString tmp{reserve_t{new_size}};
+            tmp.append(data(), index);
+            tmp.append(buf, sz);
+            tmp.append(data() + index + eff_count, size() - index - eff_count);
+
+            swap(*this, tmp);
+        };
+
+        if constexpr (!std::is_nothrow_copy_assignable_v<CharT>
+                   || !std::is_nothrow_move_assignable_v<CharT>
+                   || !std::is_nothrow_constructible_v<CharT>)
+        {
+            replace_exsafe_();
+        }
+        else if(new_size > capacity())
+        {
+            replace_exsafe_();
+        }
+        else
+        {
+            if(new_size < size())
+            {
+                // slide left
+                std::move(
+                        data() + index + eff_count,
+                        data() + size(),
+                        data() + index + sz
+                    );
+
+                // std::destroy(data() + new_size, data() + size());
+
+                *(data() + new_size) = *ptr_to_null(); // std::uninitialized_copy
+            }
+            else if(new_size > size())
+            {
+                size_t excess_size = new_size - size();
+
+                // slide right (part1: uninitialized section)
+                std::copy(
+                        std::reverse_iterator<CharT*>(data() + size()),
+                        std::reverse_iterator<CharT*>(data() + size() - excess_size),
+                        std::reverse_iterator<CharT*>(data() + new_size)
+                    ); // std::uninitialized_copy
+
+                // slide right (part2: overwrite section)
+                std::move(
+                        std::reverse_iterator<CharT*>(data() + size() - excess_size),
+                        std::reverse_iterator<CharT*>(data() + index + eff_count),
+                        std::reverse_iterator<CharT*>(data() + size())
+                    );
+
+                *(data() + new_size) = *ptr_to_null(); // std::uninitialized_copy
+            }
+
+            // do the remaining replacement in-place
+            std::copy(buf, buf + sz, data() + index); // std::copy assign
+            m_size = new_size;
+        }
+
+        return *this;
+    }
+
+    BasicString& replace(size_t index, size_t count, const CharT* buf)
+    {
+        return replace(index, count, buf, strlen_(buf));
+    }
+
+    BasicString& erase(size_t index, size_t count = npos)
+    {
+        return replace(index, count, "");
+    }
+
+    BasicString& insert(size_t index, const CharT* buf, size_t sz)
+    {
+        return replace(index, 0, buf, sz);
+    }
+
+    BasicString& insert(size_t index, const CharT* buf)
+    {
+        return insert(index, buf, strlen_(buf));
+    }
+
+    BasicString& insert(size_t index, const BasicString& str)
+    {
+        return insert(index, str.data(), str.size());
+    }
+
+    size_t find(const CharT* buf, size_t index, size_t sz) const
+    {
+        // move to trait
+
+        if(sz > size() || index > size() - sz)
+            return npos;
+
+        const CharT* beg = data() + index;
+        const CharT* end = data() + size() - sz;
+        while(true)
+        {
+            bool match = true;
+            for(size_t i = 0u; i < sz; ++i)
+            {
+                if (!std::equal_to{}(*(beg + i), *(buf + i))) // move to trait
+                {
+                    match = false;
+                    break;
+                }
+            }
+
+            if(match)
+            {
+                return beg - data();
+            }
+
+            if(beg++ == end)
+                break;
+        }
+
+        return npos;
+    }
+
+    size_t find(const CharT* buf, size_t index = 0) const
+    {
+        return find(buf, index, strlen_(buf));
+    }
+
+    size_t find(const BasicString& str, size_t index = 0) const
+    {
+        return find(str.data(), index, str.size());
     }
 
     //////////////////////////
@@ -289,6 +465,8 @@ private:
     {
         if(new_cap > max_size())
             throw std::length_error("size too big");
+
+        using std::swap;
 
         BasicString tmp{reserve_t{new_cap}};
         tmp.append(*this);
--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 
 int main()
 {
+    // constructor - default
     {
         BasicString<char> str;
-        std::cout << str.size() << "," << str.capacity() << "\n";
 
         assert( str.size() == 0u );
         assert( str.data() != nullptr );
@@ -20,11 +20,183 @@ int main()
         assert( oss.str() == "[]" );
     }
 
+    // constructor - cstring
+    {
+        const char* cstr = "Hello";
+        BasicString<char> str{cstr};
+
+        assert( str.size() == 5u );
+        assert( str.data() != nullptr );
+        assert( str.capacity() >= str.size() );
+        assert( str.empty() != true );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[Hello]" );
+    }
+
+    // constructor - array
+    {
+        const char carray[] = "Hello";
+        BasicString<char> str{carray};
+
+        assert( std::size(carray) == 6u );
+        assert( str.size() == 5u );
+        assert( str.data() != nullptr );
+        assert( str.capacity() >= str.size() );
+        assert( str.empty() != true );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[Hello]" );
+    }
+
+    // constructor - cstring with size
+    {
+        BasicString<char> str{"Hello World", 5};
+
+        assert( str.size() == 5u );
+        assert( str.data() != nullptr );
+        assert( str.capacity() >= str.size() );
+        assert( str.empty() != true );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[Hello]" );
+    }
+
+    // copy construct
+    {
+        BasicString<char> str0{"Hello"};
+        str0.reserve(50);
+        size_t srcCap = str0.capacity();
+
+        BasicString<char> str = str0; // copy construct
+
+        assert( str.size() == 5u );
+        assert( str.data() != nullptr );
+        assert( str.capacity() >= str.size() );
+        assert( str.capacity() < srcCap );
+        assert( str.empty() != true );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[Hello]" );
+    }
+
+    // move construct
+    {
+        BasicString<char> str0{"Hello"};
+        str0.reserve(50);
+        size_t srcCap = str0.capacity();
+
+        BasicString<char> str = std::move(str0); // move construct
+
+        assert( str.size() == 5u );
+        assert( str.data() != nullptr );
+        assert( str.capacity() >= str.size() );
+        assert( str.capacity() == srcCap );
+        assert( str.empty() != true );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[Hello]" );
+    }
+
+    // copy assign
+    {
+        BasicString<char> str0{"Hello New Copy World!"};
+        BasicString<char> str{"Something big!Something big!Something big!Something big!Something big!"};
+        assert( str.size() == 70 );
+
+        size_t oldCap = str.capacity();
+        str = str0; // copy assign
+
+        assert( str.size() == 21u );
+        assert( str.capacity() >= str.size() );
+        assert( str.capacity() == oldCap );
+
+        {
+            std::ostringstream oss;
+            oss << "[" << str << "]";
+            assert( oss.str() == "[Hello New Copy World!]" );
+        }
+
+        {
+            // check source object
+            std::ostringstream oss;
+            oss << "[" << str0 << "]";
+            assert( oss.str() == "[Hello New Copy World!]" );
+        }
+    }
+
+    // move assign
+    {
+        BasicString<char> str0{"Hello New Move World!"};
+        BasicString<char> str{"Something big!Something big!Something big!Something big!Something big!"};
+        assert( str.size() == 70 );
+
+        size_t oldCap = str.capacity();
+        str = std::move(str0); // move assign
+
+        assert( str.size() == 21u );
+        assert( str.capacity() == str.size() );
+        assert( str.capacity() < oldCap );
+
+        {
+            std::ostringstream oss;
+            oss << "[" << str << "]";
+            assert( oss.str() == "[Hello New Move World!]" );
+        }
+
+        {
+            // check source object
+            // note: accessing a moved from object - but this is okay as this
+            //       implementation ensures that it is in a valid state.
+            std::ostringstream oss;
+            oss << "[" << str0 << "]";
+            assert( oss.str() == "[]" );
+        }
+    }
+
+    // assign - no reallocation (reuse)
+    {
+        BasicString<char> str{"Something big!Something big!Something big!Something big!Something big!"};
+        assert( str.size() == 70 );
+
+        size_t oldCap = str.capacity();
+        str.assign("New World!");
+
+        assert( str.size() == 10u );
+        assert( str.capacity() >= str.size() );
+        assert( str.capacity() == oldCap );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[New World!]" );
+    }
+
+    // assign - reallocation
+    {
+        BasicString<char> str{"Something"};
+        assert( str.size() == 9 );
+
+        size_t oldCap = str.capacity();
+        str.assign("New World! Where no man has gone before!");
+
+        assert( str.size() == 40u );
+        assert( str.capacity() == str.size() );
+        assert( str.capacity() > oldCap );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[New World! Where no man has gone before!]" );
+    }
+
+    // append (multiple), push_back
     {
         size_t oldCap = 0u;
-        char a[] = "hello";
-        BasicString<char> str = a;
-        std::cout << str.size() << "," << str.capacity() << "\n";
+        BasicString<char> str = "hello";
 
         assert( str.size() == 5u );
         assert( str.capacity() >= str.size() );
@@ -39,7 +211,6 @@ int main()
             oldCap = str.capacity();
 
             str.append(" ");
-            std::cout << str.size() << "," << str.capacity() << "\n";
             assert( str.size() == 6u );
             assert( str.capacity() >= str.size() );
             assert( str.capacity() > oldCap );
@@ -53,7 +224,6 @@ int main()
             oldCap = str.capacity();
 
             str.append("wor");
-            std::cout << str.size() << "," << str.capacity() << "\n";
             assert( str.size() == 9u );
             assert( str.capacity() >= str.size() );
             assert( str.capacity() == oldCap );
@@ -67,7 +237,6 @@ int main()
             oldCap = str.capacity();
 
             str.append("ld");
-            std::cout << str.size() << "," << str.capacity() << "\n";
             assert( str.size() == 11u );
             assert( str.capacity() >= str.size() );
             assert( str.capacity() > oldCap );
@@ -76,75 +245,322 @@ int main()
             oss << "[" << str << "]";
             assert( oss.str() == "[hello world]" );
         }
+    }
+
+    // clear
+    {
+        BasicString<char> str{"Something big!Something big!Something big!Something big!Something big!"};
+        assert( str.size() == 70u );
+
+        size_t oldCap = str.capacity();
+        str.clear();
+
+        assert( str.size() == 0u );
+        assert( str.capacity() > str.size() );
+        assert( str.capacity() == oldCap );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[]" );
+    }
+
+    // shrink_to_fit
+    {
+        BasicString<char> str{"Something big!Something big!Something big!Something big!Something big!"};
+        assert( str.size() == 70u );
+
+        size_t oldCap = str.capacity();
+
+        str = "Hello";
+
+        assert( str.size() == 5u );
+        assert( str.capacity() > str.size() );
+        assert( str.capacity() == oldCap );
+
+        str.shrink_to_fit();
+        assert( str.size() == 5u );
+        assert( str.capacity() == str.size() );
+        assert( str.capacity() < oldCap );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[Hello]" );
+    }
+
+    // substr
+    {
+        BasicString<char> str{"This is big and that is small!"};
+        assert( str.size() == 30u );
 
         {
-            oldCap = str.capacity();
-
-            str.shrink_to_fit();
-            std::cout << str.size() << "," << str.capacity() << "\n";
-            assert( str.size() == 11u );
-            assert( str.capacity() == str.size() );
-            assert( str.capacity() < oldCap );
+            BasicString<char> str1 = str.substr(0u, 11u);
+            assert( str1.size() == 11u );
 
             std::ostringstream oss;
-            oss << "[" << str << "]";
-            assert( oss.str() == "[hello world]" );
+            oss << "[" << str1 << "]";
+            assert( oss.str() == "[This is big]" );
         }
 
         {
-            oldCap = str.capacity();
-
-            str.push_back('!');
-            std::cout << str.size() << "," << str.capacity() << "\n";
-            assert( str.size() == 12u );
-            assert( str.capacity() >= str.size() );
-            assert( str.capacity() > oldCap );
+            BasicString<char> str1 = str.substr(16u, 4u);
+            assert( str1.size() == 4u );
 
             std::ostringstream oss;
-            oss << "[" << str << "]";
-            assert( oss.str() == "[hello world!]" );
+            oss << "[" << str1 << "]";
+            assert( oss.str() == "[that]" );
         }
 
         {
-            oldCap = str.capacity();
-
-            str.assign("New World!");
-            std::cout << str.size() << "," << str.capacity() << "\n";
-            assert( str.size() == 10u );
-            assert( str.capacity() >= str.size() );
-            assert( str.capacity() == oldCap );
+            BasicString<char> str1 = str.substr(16u, 9000u);
+            assert( str1.size() == 14u );
 
             std::ostringstream oss;
-            oss << "[" << str << "]";
-            assert( oss.str() == "[New World!]" );
+            oss << "[" << str1 << "]";
+            assert( oss.str() == "[that is small!]" );
         }
 
         {
-            oldCap = str.capacity();
-
-            str.assign("New World! Where no man has gone before!");
-            std::cout << str.size() << "," << str.capacity() << "\n";
-            assert( str.size() == 40u );
-            assert( str.capacity() == str.size() );
-            assert( str.capacity() > oldCap );
+            BasicString<char> str1 = str.substr(16u);
+            assert( str1.size() == 14u );
 
             std::ostringstream oss;
-            oss << "[" << str << "]";
-            assert( oss.str() == "[New World! Where no man has gone before!]" );
+            oss << "[" << str1 << "]";
+            assert( oss.str() == "[that is small!]" );
         }
+    }
+
+    // replace - no realloc, replacement str size < replaced str size
+    {
+        BasicString<char> str{"This is a very bad world!"};
+
+        assert( str.size() == 25u );
+        size_t oldCap = str.capacity();
+
+        str.replace(10u, 8u, "great");
+
+        assert( str.size() == 22u );
+        assert( str.capacity() == oldCap );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[This is a great world!]" );
+    }
+
+    // replace - no realloc, replacement str size == replaced str size
+    {
+        BasicString<char> str{"This is a very bad world!"};
+
+        assert( str.size() == 25u );
+        size_t oldCap = str.capacity();
+
+        str.replace(10u, 8u, "mindless");
+
+        assert( str.size() == 25u );
+        assert( str.capacity() == oldCap );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[This is a mindless world!]" );
+    }
+
+    // replace - no realloc, replacement str size > replaced str size
+    {
+        BasicString<char> str{"This is a very bad world!"};
+        str.reserve(30);
+
+        assert( str.size() == 25u );
+        assert( str.capacity() == 30u );
+        size_t oldCap = str.capacity();
+
+        str.replace(10u, 8u, "multi-faceted");
+
+        assert( str.size() == 30u );
+        assert( str.capacity() == oldCap );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[This is a multi-faceted world!]" );
+    }
+
+    // replace - realloc, replacement str size > replaced str size
+    {
+        BasicString<char> str{"This is a very bad world!"};
+        str.shrink_to_fit();
+
+        assert( str.size() == 25u );
+        assert( str.capacity() == 25u );
+
+        str.replace(10u, 8u, "multi-faceted");
+
+        assert( str.size() == 30u );
+        assert( str.capacity() == 30u );
+
+        std::ostringstream oss;
+        oss << "[" << str << "]";
+        assert( oss.str() == "[This is a multi-faceted world!]" );
+    }
+
+    // erase
+    {
+        BasicString<char> str{"This is a very good world!"};
+        str.shrink_to_fit();
+
+        assert( str.size() == 26u );
+        assert( str.capacity() == 26u );
+
+        str.erase(10u, 5u);
+
+        assert( str.size() == 21u );
+        assert( str.capacity() == 26u );
 
         {
-            oldCap = str.capacity();
+            std::ostringstream oss;
+            oss << "[" << str << "]";
+            assert( oss.str() == "[This is a good world!]" );
+        }
 
-            str.clear();
-            std::cout << str.size() << "," << str.capacity() << "\n";
-            assert( str.size() == 0u );
-            assert( str.capacity() > str.size() );
-            assert( str.capacity() == oldCap );
+        str.erase(9u);
+        assert( str.size() == 9u );
+        assert( str.capacity() == 26u );
 
+        {
+            std::ostringstream oss;
+            oss << "[" << str << "]";
+            assert( oss.str() == "[This is a]" );
+        }
+
+        str.erase(7u, 3000u);
+        assert( str.size() == 7u );
+        assert( str.capacity() == 26u );
+
+        {
+            std::ostringstream oss;
+            oss << "[" << str << "]";
+            assert( oss.str() == "[This is]" );
+        }
+
+        str.erase(0u);
+        assert( str.size() == 0u );
+        assert( str.capacity() == 26u );
+
+        {
             std::ostringstream oss;
             oss << "[" << str << "]";
             assert( oss.str() == "[]" );
+        }
+    }
+
+    // insert
+    {
+        BasicString<char> str{"This is a world!"};
+        str.shrink_to_fit();
+
+        str.insert(10u, "very good ");
+
+        {
+            std::ostringstream oss;
+            oss << "[" << str << "]";
+            assert( oss.str() == "[This is a very good world!]" );
+        }
+    }
+
+    // find
+    {
+        BasicString<char> str{"This is a very good world! That is true!"};
+
+        {
+            size_t r = str.find("is", 0u);
+            assert( r == 2u );
+        }
+
+        {
+            size_t r = str.find("is", 1u);
+            assert( r == 2u );
+        }
+
+        {
+            size_t r = str.find("is", 2u);
+            assert( r == 2u );
+        }
+
+        {
+            size_t r = str.find("is", 3u);
+            assert( r == 5u );
+        }
+
+        {
+            size_t r = str.find("is", 4u);
+            assert( r == 5u );
+        }
+
+        {
+            size_t r = str.find("is", 5u);
+            assert( r == 5u );
+        }
+
+        {
+            size_t r = str.find("is", 6u);
+            assert( r == 32u );
+        }
+
+        {
+            size_t r = str.find("is", 32u);
+            assert( r == 32u );
+        }
+
+        {
+            size_t r = str.find("is", 33u);
+            assert( r == str.npos );
+        }
+
+        {
+            size_t r = str.find("is", 38u);
+            assert( r == str.npos );
+        }
+
+        {
+            size_t r = str.find("is", 39u);
+            assert( r == str.npos );
+        }
+
+        {
+            size_t r = str.find("is", 40u);
+            assert( r == str.npos );
+        }
+
+        {
+            size_t r = str.find("is", 5000u);
+            assert( r == str.npos );
+        }
+
+        {
+            size_t r = str.find("This", 0u);
+            assert( r == 0u );
+        }
+
+        {
+            size_t r = str.find("This", 38u);
+            assert( r == str.npos );
+        }
+
+        {
+            size_t r = str.find("true!", 0u);
+            assert( r == 35u );
+        }
+
+        {
+            size_t r = str.find("true!", 35u);
+            assert( r == 35u );
+        }
+
+        {
+            size_t r = str.find("true!", 36u);
+            assert( r == str.npos );
+        }
+
+        {
+            size_t r = str.find("This is a very good world! That is true!", 0u);
+            assert( r == 0u );
         }
     }
 }
